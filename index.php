3ba22af8887e6825f8affe65008497e45cddc367
Shakir Uz Zaman <shakir@grype.ca>
2022-11-20 00:47:47 +0600

    plugin intro added

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-
-
-?>
+/*
+Plugin Name: WP-LIFE_POS
+Plugin URI: #
+Description: development project
+Author: shakir
+Version: 0.0.1
+Author URI: https://www.linkedin.com/in/shakir-uz-zaman/
+*/
